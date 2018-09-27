9660f65075cc28b195ec7bc7dc0dabe0115aaf96
Philip Otieno <tmitchellb007@gmail.com>
2018-09-27 10:51:33 +0300

    ft-  git commit -m ft-

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_restful import Api
 
 import instance.config
 
@@ -6,9 +7,15 @@ def create_app(config_name):
 	'''creates the flask app depending on configurations passed'''
 
 	app  = Flask(__name__)
+	api=Api(app)
 
 
 	app.config.from_object(instance.config.app_config)
 
+    with app.app_context():
+        from app.resources.users import Register, Login
+
+    api.add_resource(Register, '/api/v1/user/signup')
+    api.add_resource(Login, '/api/v1/user/login')
 
 	return app
