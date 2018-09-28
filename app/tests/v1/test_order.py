cd5c9e0471a7c5a44d4e771684ab57c43b9db695
Philip Otieno <tmitchellb007@gmail.com>
2018-09-28 05:04:10 +0300

    Removed duplicate test"

--- app/tests/v1/test_order.py
+++ app/tests/v1/test_order.py
@@ -18,22 +18,16 @@ class Test_Order(BaseClass):
 		self.assertEqual(response.status_code, 401)
 		self.assertEqual(result['message'], 'you are not logged in/session expired')
 
-	def test_get_order_without_loged_in(self):
+	def test_get_specific_order_without_loged_in(self):
 		'''Cannot get a list of orders without login'''
-		response = self.client.post(POST_URL,
-			data=json.dumps(self.order_data), content_type = 'application/json')
-		
 		response = self.client.post(GET_SINGLE_URL,
 			data = json.dumps(self.order_data), content_type = 'application/json')
 		result = json.loads(response.data.decode())
 		self.assertEqual(response.status_code, 401)
 		self.assertEqual(result['message'], 'you are not logged in/session expired')
 
-	def test_get_specific_order_without_loged_in(self):
+	def test_get_all_order_without_loged_in(self):
 		'''Cannot get a list of orders without login'''
-		response = self.client.post(POST_URL,
-			data=json.dumps(self.order_data), content_type = 'application/json')
-		
 		response = self.client.post(GET_ALL_URL,
 			data = json.dumps(self.order_data), content_type = 'application/json')
 		result = json.loads(response.data.decode())
